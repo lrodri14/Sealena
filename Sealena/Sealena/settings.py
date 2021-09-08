ec0d05ae3abc0344a46af8cd0efa142173a9dc33
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-09-08 07:45:50 -0600

    Define path for static files collection in production

--- Sealena/Sealena/settings.py
+++ Sealena/Sealena/settings.py
@@ -179,7 +179,7 @@ DATETIME_FORMAT = '%Y-%m-%dT%H:%M'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
